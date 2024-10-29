cd4edad2ef3a0d7b2808290ea6f79a5fa32b666d
gaogo21 <ggao88299@gmail.com>
2024-10-29 23:23:45 +0800

    Time: 0 ms (100.00%) | Memory: 31 MB (10.23%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int left = 0, right = 0, sum = 0, len = INT_MAX;
-        while(right < nums.size())
+        int res = INT_MAX, sum = 0;
+        for (int left = 0, right = 0; right < nums.size(); ++ right)
         {
+            //1.进窗口
             sum += nums[right];
+
+            //2.出窗口 --> 有条件
             while(sum >= target)
             {
-                //更新结果
-                len = min(len, (right - left + 1));
+                //3.更新结果
+                res = min(res, right - left + 1);
                 sum -= nums[left];
                 ++left;
             }
-
-            ++right;
         }
 
-        return len == INT_MAX ? 0 : len;
+        //如果res没有被改变，说明整个数组加起来都没有target大，因此返回0
+        if (res == INT_MAX)
+            return 0;
+        return res;
     }
 };
